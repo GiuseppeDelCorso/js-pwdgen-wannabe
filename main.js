c665338c00934874dae862f1cfafa8326a3c770f
GiuseppeDelCorso <143078096+GiuseppeDelCorso@users.noreply.github.com>
2023-10-26 18:32:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let nomeUtente = prompt( "Inserisci il tuo Nome");
+let congomeUtente = prompt("Inserisci il tuo Congome");
+let colorePreferito = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("password").innerHTML = "La tua Password è, " + nomeUtente + congomeUtente + colorePreferito + "#23";
